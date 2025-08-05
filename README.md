0ed4b0b42bb184275e54548699439892d5cea8dc
Prasad Mujumdar <prasad@okahu.ai>
2025-08-04 22:53:56 -0700

    Okahu demo agent. Initial setup

---
+++ README.md
@@ -0,0 +1,41 @@
+# Okahu agent demo with OpenAI + Langgraph
+This repo includes a demo agent application built using OpenAI & Langgraph that is pre-instrumented for observation with Okahu AI Observability cloud. 
+You can fork this repo and run the app in Github Codespaces or laptop/desktop to get started quickly.
+
+## Try Okahu with this Agentic app
+
+To try this agent 
+- Fork this repo and run in the Github Codespace 
+- To run this on Mac 
+  - Install python dependencies: ```pip install -r requirement.txt```
+  - Remove libmagic: ```pip uninstall python-magic```
+- To run this on Windows
+  - Install python dependencies: ```pip install -r requirement.txt```
+
+You'll need 
+- An OpenAI subscription and an API key to [OpenAI developer platform](https://platform.openai.com/overview)
+- An Okahu tenant and API key to [Okahu AI Observability Cloud](https://www.okahu.co)
+  - Sign up for Okahu AI accout with your LinkedIn for Github ID
+  - Once you login, nagivate to 'Settings' on the left navigation bar and click on 'Generate Okahu API Key'
+  - Copy the API key generated and save. Note that you'll not be able to extract that API key after you navigate away from that page.
+
+## Configure the demo environment
+- Copy the file agent_env.sh.template to agent_env.sh
+- Edit the agent_env.sh file to add the OpenAI API Key and Okahu API key and save
+
+## Run the travel agent app 
+This application is an travel agent app that mocks travel related tasks like flight booking, hotel booking and checking weather in a city.
+It's is a python program using Langgraph Agent framework. 
+The app uses OpenAI gpt-4o model for inference.
+
+1. Source the agent environment
+  ```source agent_env.sh```
+2. Start the mock weather MCP server
+  ```python weather-mcp-server.py > mcp.out 2>&1 &```
+3. Verify mcp server is running
+  ```cat mcp.out```
+  The above command prints content of mpc.out file which should show a message `Application startup complete`
+4. Run the pre-instrumented travel agent app with following command
+   ```python lc-travel-agent.py```
+   The application will prompt you for a travel booking task. You can enter something like `Book a flight from SFO to BOM next week. Book Marriot hotel in central mumbai. Also how't the weather going to be in Mumbai next week?`
+   It should responds with successful booking of flight and hotel, as well as weather forcast.
---
+++ lg-travel-agent.py
@@ -0,0 +1,100 @@
+# Enable Monocle Tracing
+from monocle_apptrace import setup_monocle_telemetry
+setup_monocle_telemetry(workflow_name = 'okahu-demo-lg-travel-agent', monocle_exporters_list = 'okahu')
+
+import asyncio
+import os
+import time
+from langchain_core.messages import HumanMessage, ToolMessage
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from langgraph_supervisor import create_supervisor
+from langchain_core.tools import tool
+from langchain_mcp_adapters.client import MultiServerMCPClient
+
+import logging
+logger = logging.getLogger(__name__)
+DEFAULT_PORT = 8002
+port = int(os.getenv("PORT", DEFAULT_PORT))
+
+@tool("okahu-demo-lg-tool_book_hotel", description="Book a hotel for a stay")
+def book_hotel(hotel_name: str):
+    """Book a hotel"""
+    return f"Successfully booked a stay at {hotel_name}."
+
+@tool("okahu-demo-lg-tool_book_flight", description="Book a flight from one airport to another")
+def book_flight(from_airport: str, to_airport: str):
+    """Book a flight"""
+    return f"Successfully booked a flight from {from_airport} to {to_airport}."
+
+# Set up MCP client for monocle repo
+async def get_mcp_tools():
+    """Get tools from the monocle MCP server."""
+    client = MultiServerMCPClient(
+        {
+            "weather": {
+                "url": f"http://localhost:{port}/weather/mcp/",
+                "transport": "streamable_http",
+            }
+        }
+    )
+    # get list of all tools from the MCP server and their descriptions
+    tools = await client.get_tools()
+    return tools
+
+# Set up agents for travel booking
+async def setup_agents():
+
+    weather_tools = await get_mcp_tools()
+
+    flight_assistant = create_react_agent(
+        model="openai:gpt-4o",
+        tools=[book_flight],
+        prompt="You are a flight booking assistant",
+        name="okahu-demo-lg-agent-air_travel_assistant"
+    )
+
+    hotel_assistant = create_react_agent(
+        model="openai:gpt-4o",
+        tools=[book_hotel],
+        prompt="You are a hotel booking assistant",
+        name="okahu-demo-lg-agent-lodging_assistant"
+    )
+
+    weather_agent = create_react_agent(
+        model="openai:gpt-4o",
+        tools=weather_tools,
+        prompt="You are a weather information assistant. Please use the tool available to you for checking weather. Extract city name from the user query and pass it to the weather tool.",
+        name="okahu-demo-lg-agent-weather_assistant"
+    )
+    supervisor = create_supervisor(
+        supervisor_name="okahu-demo-lg-agent-travel_supervisor",
+        agents=[flight_assistant, hotel_assistant, weather_agent],
+        model=ChatOpenAI(model="gpt-4o"),
+        prompt=(
+            "You manage a hotel booking assistant and a"
+            "flight booking assistant. Assign work to them."
+            "If the user asks for weather information, delegate to the weather assistant."
+        )
+    ).compile()
+
+    return supervisor
+
+# Run the agent with a user request
+async def run_agent(request: str):
+    supervisor = await setup_agents()
+    chunk = await supervisor.ainvoke(
+        input={
+            "messages": [
+                {
+                    "role": "user",
+                    "content": request
+            }
+        ]
+    })
+    print(chunk["messages"][-1].content)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.WARN)
+    request = input("\nI am a travel booking agent. How can I assist you with your travel plans? (You can ask me to book flights, hotels, or check the weather at any location.): ")
+    asyncio.run(run_agent(request))
